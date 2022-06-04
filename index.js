8c55ff5616a3a38c6fdb2e776a1f560d1c60203f
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2022-06-03 21:59:38 -0400

    added controller, models and routes to products

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+const productRouter = require('./routes/Products')
 
-//GET /products/all?page=2&sort=price&order=desc HTTP/1.1
-app.get('/products/all',(request, response) => {
-    response.send(
-       'Products,' + request.query.page + ',' + request.query.sort + ',' + request.query.order
-    )
-})
-
-//GET /products/8719-small/red HTTP/1.1
-app.get('/products/:productId-:size-:color',(request, response) => {
-    response.send(
-        'Product,' + request.params.productId +
-        ',' + request.params.size +
-        ',' + request.params.color
-    )
-})
-
-//GET /products/8719 HTTP/1.1
-app.get('/products/:productId',(request, response) => {
-    response.send(
-        'Product, ' + request.params.productId
-        )
-})
-
+app.use('/products', productRouter)
 
 
 app.listen(3000)
