43eb9ba78e7e5fff83d0fc4334c9a9ec5e20b3a4
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2022-06-11 20:28:14 -0400

    RESTful Template Structure

--- controllers/Products.js
+++ controllers/Products.js
@@ -1,31 +1,34 @@
 const Products = require('../models/Products')
 
 const index = (req, res) => {
-    // const products = Products.all()
-    // res.json(products)
+    const products = Products.all()
+    res.render('products/index', { products })
 
     
 }
 
 const form = (req, res) => {
-    res.send(
-        'Products.form'
-    )
+    if(req.params.id){
+        const product = Products.find(req.params.id)
+        res.render('products/edit', { product })
+    } else {
+        res.render('products/create')
+    }
 }
 
 const show = (req, res) => {
     const product = Products.find(req.params.id)
-    res.json(product)
+    res.render('products/show', { product })
 }
 
 const create = (req, res) => {
     const product = Products.create(req.body)
-    res.json(product)
+    res.redirect('products/' + product.id)
 }
 
 const update = (req, res) => {
     const product = Products.update(req.params.id, req.body)
-    res.json(product)
+    res.redirect('/products/' + req.params.id)
 }
 
 const remove = (req, res) => {
