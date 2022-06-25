2ee47208fea82dd2b7d0c62ea80e70a0e4e07d52
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2022-06-25 19:41:41 -0400

    Working on getting slugs to work

--- migrations/20220619120845-create-image.js
+++ migrations/20220619120845-create-image.js
@@ -15,6 +15,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
+      extension: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
--- migrations/20220620021705-modify_users_add_new_fields.js
+++ migrations/20220620021705-modify_users_add_new_fields.js
@@ -8,22 +8,6 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-     queryInterface.addColumn(
-      'Variants', // table name
-      'inventory', // new field name
-      {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-      },
-    ),
-    queryInterface.addColumn(
-      'Variants',
-      'description',
-      {
-        type: Sequelize.TEXT('long'),
-        allowNull: true,
-      },
-    )
   },
 
   async down (queryInterface, Sequelize) {
@@ -33,7 +17,5 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-     queryInterface.removeColumn('Variants', 'description'),
-     queryInterface.removeColumn('Variants', 'inventory')
   }
 };
